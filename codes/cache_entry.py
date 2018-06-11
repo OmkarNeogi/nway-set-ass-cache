dbbb49b8f5dbde8690798ce008742c0e2374c702
omkar <omkarneogi2@gmail.com>
2018-06-11 01:38:50 -0400

    Reworked according to third set of Tim's suggestions

--- codes/cache_entry.py
+++ codes/cache_entry.py
@@ -1,24 +1,10 @@
-from abc import ABC
-
-
-class CacheEntry(ABC):
-    """
-    Abstract class for all CacheEntry objects.
-    """
-
-    def __init__(self, key, val):
+class CacheEntry(object):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
 
     def __repr__(self):
-        # All values are expected have a repr function.
-        # For custom objects this will not work
-        try:
-            _ = str(self.val)
-        except NotImplementedError:
-            return "Implement __repr__ for values of NSACache"
-        # str(self.key) is expected to be of an immutable type
-        return "CacheEntry(" + str(self.key) + "," + str(self.val) + ")"
+        return "CacheEntry(" + str(self.key) + "," + str(self.value) + ")"
 
     def __eq__(self, other):
         return self.key == other.key
@@ -28,23 +14,12 @@ class CacheEntry(ABC):
 
 
 class DLLCacheEntry(CacheEntry):
-    def __init__(self, key, val):
-        super().__init__(key, val)
-        self.prev = None
+    def __init__(self, key, value):
+        super().__init__(key, value)
+
         self.next = None
+        self.prev = None
 
     def disconnect(self):
-        """
-        Remove the connections of this DLLCacheEntry (with the next and
-        previous nodes) by joining the next and previous DLLCacheEntry
-        nodes to each other.
-
-        :return: None
-        """
-        self.prev.next = self.next
         self.next.prev = self.prev
-
-
-class HeapCacheEntry(CacheEntry):
-    def __init__(self, key, val):
-        super().__init__(key, val)
+        self.prev.next = self.next
